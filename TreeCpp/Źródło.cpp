221fb86fde709a600b0012cb35164f027707fb36
adamkot <a.kotus@gmail.com>
2018-12-10 16:07:01 +0100

    delete from tree

--- TreeCpp/Źródło.cpp
+++ TreeCpp/Źródło.cpp
@@ -1,5 +1,6 @@
 #include <iostream> 
 #include <string>
+#include <iomanip> 
 using namespace std;
 
 class tree
@@ -14,13 +15,13 @@ private:
 		item* right = NULL;
 		item* up = NULL;
 	};
-	item* root; //korzen drzewa
+	
 
 public:
-	//void search() //znajdz
+	item* root; //korzen drzewa
 	void add(int text);
 	void del(int text);
-	string treeToString();
+	void treeToString(item* root, int indent);
 };
 
 //dodawanie do listy (na koñcu)
@@ -88,7 +89,7 @@ void  tree::add(int text)
 			}
 			else if (x->left != NULL && x->right != NULL)
 			{
-				//dodaæ optymalizacje drzewa
+				//tu ewentualnie mo¿na dodaæ optymalizacje drzewa
 				
 				if ((abs(x->left->z - text)) > (abs(x->right->z - text)))
 				{
@@ -128,25 +129,47 @@ void tree::del(int text)
 				{
 					if (x->left == NULL && x->right == NULL)
 					{
+						if (x->up->left->z == text)
+						{
+							(x->up)->left = NULL;
+						}
+						else
+						{
+							(x->up)->right = NULL;
+						}
 						x = NULL;
 					}
 					else if (x->left == NULL && x->right != NULL)
 					{
-						x = x->right;
+						if (x->up->left->z == text)
+						{
+							(x->up)->left = x->right;
+						}
+						else
+						{
+							(x->up)->right = x->right;
+						}
 					}
 					else if (x->left != NULL && x->right == NULL)
 					{
-						x = x->left;
+						if (x->up->left->z == text)
+						{
+							(x->up)->left = x->left;
+						}
+						else
+						{
+							(x->up)->right = x->left;
+						}
 					}
 					else if (x->left != NULL && x->right != NULL)
 					{
-						//tego jeszcze nie wiem
+						//przepisac drzewo
 					}
 					break;
 				}
 				else
 				{
-					if ((abs(x->left->z - text)) > (abs(x->right->z - text)))
+					if (text > x->z)
 					{
 						x = x->right;
 					}
@@ -164,33 +187,40 @@ void tree::del(int text)
 	}
 }
 
-string tree::treeToString()
+void tree::treeToString(item* root, int indent)
 {
-	string treeText = "";
-	if (!empty)
+	if (!empty) 
 	{
-		treeText = root->z + " \n";
-		item* x = root->left;
-		item* y = root->right;
-		
-		// do zrobienia
+		item* x = root;
+		if (x->right) {
+			treeToString(x->right, indent + 4);
+		}
+		if (indent) {
+			cout << setw(indent) << ' ';
+		}
+		if (x->right) cout << " /\n" << setw(indent) << ' ';
+		cout << x->z << "\n ";
+		if (x->left) {
+			cout << setw(indent) << ' ' << " \\\n";
+			treeToString(x->left, indent + 4);
+		}
 	}
-	return treeText;
 }
 
 int main()
 {
-	tree *t = new tree; // nowa drzewo
-	t->add(0); //dodaj do drzewa
-	t->add(1); //dodaj do drzewe kolejny
-	t->add(2); //dodaj do drzewa kolejny
-	t->add(3); //dodaj do drzewa kolejny
-	t->add(4); //dodaj do drzewe kolejny
-	t->add(5); //dodaj do drzewa kolejny
-	t->add(6); //dodaj do drzewa kolejny
-	cout << t->treeToString() << endl; //wypisz drzewo
-	//t->del("test_1");
-	//cout << t->treeToString() << endl; //wypisz drzewo
+	tree *t = new tree; // nowe drzewo
+	t->add(5); //dodaj do drzewa
+	t->add(2);
+	t->add(6);
+	t->add(3);
+	t->add(8); 
+	t->add(4);
+	t->add(9); 
+	t->treeToString(t->root, 1); //wypisz drzewo
+	cout << "----------------------------------" << endl;
+	t->del(9); //usuñ z drzewa
+	t->treeToString(t->root, 1);
 	getchar();
 	return 0;
 }
