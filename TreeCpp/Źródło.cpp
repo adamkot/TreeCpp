a11cc7a07e4e6ac8e1543989855e64991ba84b32
adamkot <a.kotus@gmail.com>
2018-12-11 12:38:40 +0100

    ready

--- TreeCpp/Źródło.cpp
+++ TreeCpp/Źródło.cpp
@@ -10,10 +10,10 @@ private:
 	//do przechowywania "obiektu" w drzewie. Przechowuje string i wskazania na lewy i prawy obiekt oraz wskazanie na rodzica
 	struct item
 	{
-		int z;
-		item* left = NULL;
-		item* right = NULL;
-		item* up = NULL;
+		int z; //przechowywana wartoœæ
+		item* left = NULL; //lewe dziecko
+		item* right = NULL; //prawe dziecko
+		item* up = NULL; //rodzic
 	};
 	
 
@@ -24,9 +24,12 @@ public:
 	void treeToString(item* root, int indent);
 };
 
-//dodawanie do listy (na koñcu)
+/*
+Dodawanie do drzewa
+*/
 void  tree::add(int text)
 {
+	//jeœli lista jest pusta tworzy now¹ listê
 	if (empty)
 	{
 		item* i = new item;
@@ -37,42 +40,57 @@ void  tree::add(int text)
 		root = i;
 		empty = false;
 	}
+	//jeœli lista nie jest pusta
 	else
 	{
+		//nowy liœæ
 		item* i = new item;
 		i->z = text;
 		i->left = NULL;
 		i->right = NULL;
 
+		//przeszukiwanie drzewa w poszukiwaniu miejsca na nowy liœæ
+		//zaczynam od korzenia root
 		for (item* x = root; x != NULL;)
 		{
+			//ga³¹Ÿ nie ma liœci
 			if (x->left == NULL && x->right == NULL)
 			{
+				//jeœli wartoœæ do dodania jest mniejsza ni¿ rodzic to na lewo
 				if (text < x->z)
 				{
 					x->left = i;
 				}
+				//jeœli wartoœæ do dodania jest wiêksza ni¿ rodzic to na prawo
 				else
 				{
 					x->right = i;
 				}
+				//zapisanie rodzica
 				i->up = x;
 				break;
 			} 
+			//ga³¹Ÿ ma tylko jeden liœæ z prawej strony
 			else if (x->left == NULL && x->right != NULL)
 			{
+				//sprawdŸ czy wartoœæ po prawej jest mniejsza
+				//jeœli tak to dodaj zamieñ miejscami i dodaj now¹
 				if (x->right->z < i->z)
 				{
 					x->left = x->right;
 					x->right = i;
 				}
+				//jeœli wartoœæ jest wiêksza to zapisuj z lewej
 				else 
 				{
 					x->left = i;
 				}
+				//zapisanie rodzica
 				i->up = x;
 				break;
 			}
+			//ga³¹Ÿ ma tylko jeden liœæ z lewej strony
+			//odwrotnie ni¿ w warunku powy¿ej
 			else if (x->left != NULL && x->right == NULL)
 			{
 				if (x->left->z > i->z)
@@ -87,10 +105,12 @@ void  tree::add(int text)
 				i->up = x;
 				break;
 			}
+			//ga³¹Ÿ ma dwa liœcie wiêc muszê iœæ do kolejnej ga³êzi
 			else if (x->left != NULL && x->right != NULL)
 			{
 				//tu ewentualnie mo¿na dodaæ optymalizacje drzewa
 				
+				//zasada wyboru kierunku drogi po ga³êziach
 				if ((abs(x->left->z - text)) > (abs(x->right->z - text)))
 				{
 					x = x->right;
@@ -103,14 +123,17 @@ void  tree::add(int text)
 		}
 	}
 }
-
+/*
+Usuwanie z drzewa
+*/
 void tree::del(int text)
 {
+	//usuwam jeœli drzewo nie jest puste
 	if (!empty)
 	{
+		//jeœli w drzewie jest tylko jeden element
 		if (root->left == NULL && root->right == NULL)
 		{
-			//jedyny
 			if (text == root->z)
 			{
 				root = NULL;
@@ -123,13 +146,17 @@ void tree::del(int text)
 		}
 		else 
 		{
+			//przeszukiwanie drzewa w poszukiwaniu elementu do uzuniêcia
 			for (item* x = root; x != NULL;)
 			{
+				//jeœli trafiliœmy na element
 				if (x->z == text)
 				{
+					//jeœli to liœæ to usuwam go wraz z odwo³aniami do niego z rodzica (x->up)
 					if (x->left == NULL && x->right == NULL)
 					{
-						if (x->up->left->z == text)
+						//sprawdzam czy dla rodzica znaleziony element jest lewym czy prawym dzieckiem
+						if ((x->up)->left->z == text)
 						{
 							(x->up)->left = NULL;
 						}
@@ -139,6 +166,7 @@ void tree::del(int text)
 						}
 						x = NULL;
 					}
+					//jeœli element to ga³¹Ÿ z jednym liœciem to usuwam i przesuwam liœæ wy¿ej
 					else if (x->left == NULL && x->right != NULL)
 					{
 						if (x->up->left->z == text)
@@ -150,6 +178,7 @@ void tree::del(int text)
 							(x->up)->right = x->right;
 						}
 					}
+					//jeœli element to ga³¹Ÿ z jednym liœciem to usuwam i przesuwam liœæ wy¿ej
 					else if (x->left != NULL && x->right == NULL)
 					{
 						if (x->up->left->z == text)
@@ -161,12 +190,126 @@ void tree::del(int text)
 							(x->up)->right = x->left;
 						}
 					}
+					//element to ga³¹Ÿ o dwóch dzieciach
 					else if (x->left != NULL && x->right != NULL)
 					{
-						//przepisac drzewo
+						//jeœli element nie jest korzeniem
+						if (x->up != NULL)
+						{
+							//jeœli dla rodzica jest z lewej strony
+							if ((x->up)->left->z == text)
+							{
+								item* i = x->right;
+								item* j = x->left;
+								//dziecko z lewej przypisz wy¿ej
+								(x->up)->left = x->left;
+								free(x);
+								//szukamy gdzie dopisaæ dzieci z prawej od usuniêtego elementu
+								while (j != NULL)
+								{
+									if (i->z > j->z)
+									{
+										if (j->right != NULL)
+										{
+											j = j->right;
+										}
+										else
+										{
+											j->right = i;
+											break;
+										}
+									}
+									else
+									{
+										if (j->left != NULL)
+										{
+											j = j->left;
+										}
+										else
+										{
+											j->left = i;
+											break;
+										}
+									}
+								}
+							}
+							else
+							{
+								item* i = x->left;
+								item* j = x->right;
+								//dziecko z prawej przypisz wy¿ej
+								(x->up)->right = x->right;
+								free(x);
+								//szukamy gdzie dopisaæ dzieci z lewej od usuniêtego elementu
+								while (j != NULL)
+								{
+									if (i->z > j->z)
+									{
+										if (j->right != NULL)
+										{
+											j = j->right;
+										}
+										else
+										{
+											j->right = i;
+											break;
+										}
+									}
+									else
+									{
+										if (j->left != NULL)
+										{
+											j = j->left;
+										}
+										else
+										{
+											j->left = i;
+											break;
+										}
+									}
+								}
+							}
+						}
+						//jeœli element jest korzeniem
+						else
+						{
+							root = x->left;
+							item* i = x->right;
+							item* j = new item;
+							j = root;
+							while (j != NULL)
+							{
+								if (i->z > j->z)
+								{
+									if (j->right != NULL)
+									{
+										j = j->right;
+									}
+									else
+									{
+										j->right = i;
+										break;
+									}
+								}
+								else
+								{
+									if (j->left != NULL)
+									{
+										j = j->left;
+									}
+									else
+									{
+										j->left = i;
+										break;
+									}
+								}
+							}
+						}
+						
 					}
 					break;
 				}
+				//jeœli nie trafiliœmy to szukamy dalej
 				else
 				{
 					if (text > x->z)
@@ -186,7 +329,9 @@ void tree::del(int text)
 		cout << "drzewo jest puste" << endl;
 	}
 }
-
+/*
+Drukowanie drzewa na ekranie od lewej do prawej, korzeñ na pocz¹tku i na œrodku ekranu
+*/
 void tree::treeToString(item* root, int indent)
 {
 	if (!empty) 
@@ -219,7 +364,7 @@ int main()
 	t->add(9); 
 	t->treeToString(t->root, 1); //wypisz drzewo
 	cout << "----------------------------------" << endl;
-	t->del(9); //usuñ z drzewa
+	t->del(2); //usuñ z drzewa
 	t->treeToString(t->root, 1);
 	getchar();
 	return 0;
